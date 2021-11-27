fbe063fd4339abe90ee34a610f040f1190c75bb8
Dave C <dc487@github.com>
2021-11-27 16:59:06 +0000

    Update puzzle bootstrap to include input loading code

--- setup_puzzle.py
+++ setup_puzzle.py
@@ -6,5 +6,12 @@ import sys
 if __name__ == "__main__":
     puzzle_name = sys.argv[1]
     pathlib.Path(puzzle_name).mkdir()
-    pathlib.Path(puzzle_name + "/input").mkdir()
-    pathlib.Path(puzzle_name + "/" + puzzle_name + ".py").write_text("import pathlib\n\n\nif __name__ == \"__main__\":\n\n")
+    pathlib.Path(puzzle_name + "/input.txt").touch()
+    pathlib.Path(puzzle_name + "/" + puzzle_name + ".py").write_text("""import pathlib
+
+def load_input():
+    return pathlib.Path("./input.txt").read_text().strip("\\n").splitlines()
+
+if __name__ == "__main__":
+    input = load_input()
+""")
